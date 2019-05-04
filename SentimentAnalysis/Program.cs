bce5f01892f1f5e8808fc6c46ea18ccb5b5857ae
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2019-05-04 16:01:52 -0500

    added features example

--- SentimentAnalysis/Program.cs
+++ SentimentAnalysis/Program.cs
@@ -5,15 +5,35 @@ namespace SentimentAnalysis
 {
     class Program
     {
+
+        static void PositiveOrNegative(string[] lexicon, string[] Tweets)
+        {
+            double positive = 0;
+            double negative = 0;
+
+            foreach (var tweet in Tweets)
+            {
+
+                foreach (var word in lexicon)
+                {
+                    var first = word.Split(',');
+                    if (first[0] == tweet)
+                    {
+                        Double.TryParse(first[1], out double temp);
+                        if (temp > 0)
+                            positive += temp;
+                        else
+                            negative -= temp;
+
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            var directory = Directory.GetFiles("Assets");
-            foreach (var file in directory)
-            {
-                var text = File.ReadAllLines(file);
-                Console.WriteLine($"{ string.Join('\n', text) }");
-
-            }
+            var lexicon = File.ReadAllLines("Assets/lexicon.txt");
+            var adverbs = File.ReadAllLines("Assets/adverbs_clean.csv");
+            var training = File.ReadAllLines("Tweets/training.txt");
 
             Console.WriteLine("\nPress enter to exit...");
             Console.ReadKey();
